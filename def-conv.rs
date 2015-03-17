ff75b3e14f5f378ed0e517cbdf1871800e1dc94b
urubi <urubi@users.noreply.github.com>
2015-03-17 11:36:53 +0300

    Create def-conv.rs
    Lets see how well online editing works.

---
+++ def-conv.rs
@@ -0,0 +1,126 @@
+//usr/bin/env true; E=`mktemp`|| exit 1; rustc -o "$E" "$0" || exit 1; "$E" "$@"; R=$?; rm "$E"; exit $R;
+/*
+    
+    The dumb C-defs to Rust constants Conversion Script
+    Copyright (C) 2015  Urubi of Kuwait
+    
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the [ GNU Lesser General Public License ] as 
+    published by the Free Software Foundation, either version 3 of the 
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    ---------------------------------------------------------------------
+    Note: Either compile is normally or chmod +x the file and run it as a 
+    script. This should work with shells on real unix systems.
+    ---------------------------------------------------------------------
+*/
+
+#![feature(io)]
+#![feature(str_words)]
+
+
+const INFO: &'static str = "The dumb C-defs to Rust constants Conversion Script
+
+Example:
+    $echo '#define SOMECONST 12' | ./def-conv.rs --type=isize 
+    # Result: pub const SOMECONST:isize = 12;
+
+Options:
+    --help                      This message.
+    --type=TYPE                 What type to define the constants as. (Required)
+    --qualifier=QUALIFIER(S)    What qualifiers to use for the constant. 
+                                (Defaults to 'pub const')
+Bugs:
+    https://github.com/urubi
+
+License: 
+    LGPLv3 (see source.)
+";
+
+fn main() {
+    use std::io;
+    use std::env;
+    use std::io::BufReadExt;
+    use std::io::Write;
+    use std::str::StrExt;
+    
+    let mut def_type: Option<String>;       // def_type: "u8", "&str", ...
+    let mut def_qualifier: Option<String>;  // def_qualifier: "pub const", ...
+    
+    let stdin = io::stdin();
+    let mut stderr = io::stderr();
+    
+    def_type = None;
+    def_qualifier = Some(format!("pub const"));
+    
+    
+    
+    // Parsing command line arguments
+    for arg in env::args().skip(1) {
+        let arg: &str = &arg;
+        if &arg[..1] == "-" {
+            let pair: Vec<&str> = arg.split('=').collect();
+            if pair[0] == "--help" {
+                println!("{}", INFO);
+                return;
+            }
+            else if pair[0] == "--type" {
+                if pair.len() != 2 {
+                    stderr.write_all("usage: --type='TYPE'\n".as_bytes()).unwrap();
+                    return;
+                }
+                def_type = Some(pair[1].to_string());
+            }
+            else if pair[0] == "--qualifier" {
+                if pair.len() != 2 {
+                    stderr.write_all("usage: --qualifier='TYPE'\n".as_bytes()).unwrap();
+                    return;
+                }
+                def_qualifier = Some(pair[1].to_string());
+            }
+            else {
+                stderr.write_all(format!("unknown option '{}'. type --help for more information.\n", pair[0]).as_bytes()).unwrap();
+                return;
+            }
+        }
+    }
+
+    if def_type.is_none() {
+        stderr.write_all("Const specification required. use --type\n".as_bytes()).unwrap();
+        return;
+    }
+    
+    
+    let def_type = def_type.unwrap();
+    let def_qualifier = def_qualifier.unwrap();
+    
+    
+    for line in stdin.lock().lines() {
+        let mut line: String = line.unwrap();
+        
+        // Check if line is long enough and is a definition
+        if line.len() <= 7 || &line[..7] != "#define" {continue;}
+
+        // stripping comments
+        line = line.split("/*").collect::<Vec<&str>>()[0].to_string();
+        line = line.split("//").collect::<Vec<&str>>()[0].to_string();
+        
+        // segmenting line
+        let words: Vec<&str> = line.words().collect();
+        if words.len() < 3 {continue;}
+        
+        // building rust equivalent
+        println!("{}{};", 
+            format!("{} {}:{} = ", &def_qualifier, words[1], &def_type), 
+            words[2..].connect(" ")
+        );
+    }
+}
